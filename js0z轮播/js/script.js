f25b85a9ccc2b2d0a8fe7f02d3fd09a79b16cdef
losthj <thislost@gmail.com>
2019-09-11 18:00:01 +0800

    update lunbo

---
+++ js0z轮播/js/script.js
@@ -0,0 +1,79 @@
+﻿//封装一个代替getElementById()的方法
+function byId(id) {
+    return typeof (id) === "string" ? document.getElementById(id) : id;
+}
+//console.log(byId("main"));
+//console.log(document.getElementById("main"));
+
+//全局变量
+var index = 0,
+    timer = null,
+    pics = byId("banner").getElementsByTagName("div"),
+    dots = byId("dots").getElementsByTagName("span"),
+    prev = byId("prev"),
+    next = byId("next"),
+    len = pics.length;
+//console.log(len)
+function slideImg() {
+    var main = byId("main");
+    //滑过清除定时器，离开继续
+    main.onmouseover = function () {
+        //清除定时器
+        if (timer) clearInterval(timer);
+    }
+    main.onmouseout = function () {
+        timer = setInterval(function () {
+            index++;
+            if (index >= len) {
+                index = 0;
+            }
+            //切换图片
+            changImg()
+        },2000)
+    }
+    //自动在main上触发鼠标离开事件
+    main.onmouseout();
+
+    //遍历所有圆点，并且绑定点击事件，点击圆点切换图片
+    for (var d = 0; d < len; d++) {
+        //给索引span添加一个id属性，值为d，作为当前的索引
+        dots[d].id = d;
+        dots[d].onclick = function () {
+            //改变index为当前span的索引
+            index = this.id;    
+            //调用changImg，实现切换图片
+            changImg()
+            //alert(d);//3 错误的
+        }
+    }
+
+    //下-张
+    next.onclick = function () {
+        index++;
+        if (index >= len) {
+            index = 0;
+            //console.log(index)
+        }
+        changImg();
+    }
+    //上-张
+    prev.onclick = function () {
+        index--;
+        if (index < 0) index = len-1;
+        changImg();
+    }
+}
+
+//切换图片
+function changImg() {
+    //遍历banner下所有的div，将其隐藏
+    for (var i = 0; i < len; i++) {
+        pics[i].style.display = "none";
+        dots[i].className = "";
+    }
+    
+    //根据index索引找到当前div和dots.span，将其显示出来和设为当前
+    pics[index].style.display = "block";
+    dots[index].className = "active";
+}
+slideImg();
---
+++ js0z轮播/js/script1.js
@@ -0,0 +1,72 @@
+﻿//封装一个获取id的函数
+function byId(id) {
+    return typeof(id) === "string" ? document.getElementById(id) : id;
+}
+
+var index = 0,
+    timer = null,
+    main = byId("main"),
+    banner = byId("banner"),
+    imgs = banner.getElementsByTagName("div"),
+    prev = byId("prev"),
+    next = byId("next"),
+    dots = byId("dots").getElementsByTagName("span"),
+    len = imgs.length;
+
+function slieImg() {
+// 鼠标移入暂停切换
+    main.onmouseover = function () {
+        if (timer) clearInterval(timer);
+    }
+// 鼠标移出重新开始切换
+    main.onmouseout = function () {
+        timer = setInterval(function () {
+            index++;
+            if (index >= len) {
+                index = 0;
+            }
+            changImg()
+        }, 2000)
+    }
+
+    //自动切换
+    main.onmouseout();
+    //上一张
+    prev.onclick = function () {
+        index--;
+        if (index < 0) {
+            index = len - 1;
+        }
+        changImg();
+    }
+    //下一张
+    next.onclick = function () {
+        index++;
+        if (index >= len) {
+            index = 0;
+        }
+        changImg();
+    }
+
+    //圆点切换
+    for (var j = 0; j < len; j++) {
+        dots[j].id = j;//可以用特定属性index,id
+        dots[j].onclick = function () {
+            index = this.id;
+            changImg();
+        }
+    }
+}
+
+  
+
+function changImg() {
+    for (var i = 0; i < len; i++) {
+        imgs[i].style.display = "none"
+        dots[i].className = "";
+    }
+    imgs[index].style.display = "block";
+    dots[index].className = "active";
+}
+
+slieImg()
